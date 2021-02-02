a6463ccf570dcaf8058c186edd4f709cc972555d
Manolo Battista <manolo@arkemis.it>
2021-02-02 15:27:39 +0100

    Add yarn - prettier - fix code

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,7 +3,10 @@ import LoginForm from './components/loginForm'
 import axios from 'axios'
 
 function Login() {
-
+  const [randomUserData, setRandomUserData] = useState('')
+  const [user, setUser] = useState({name:"", email:""})
+  const [error, setError] = useState("");
+  const {name, email} = user;
 
   const url = 'https://randomuser.me/api';
 
@@ -17,33 +20,20 @@ function Login() {
     }
   }
 
-  const [randomUserData, setRandomUserData] = useState('')
+  const Login = details => {
+    const { email, password, name } = details;
+    const adminUser = {
+      email: 'a@a',
+      password: 'admin'
+    }
 
-
-
-
-
-  const adminuser = {
-    email: 'a@a',
-    password: 'admin'
-  }
-  
-    const [user, setUser] = useState({name:"", email:""})
-    const [error, setError] = useState("");
-
-    const Login = details => {
-      console.log(details);
-
-      if(details.email === adminuser.email && details.password === adminuser.password){
-        console.log("logged in")
-        setUser({
-          name: details.name,
-          email: details.email
-        })
-      }else{
-        console.log('details wrong')
-        setError("Details do not match")
-      }
+    if(email === adminUser.email && password === adminUser.password){
+      console.log("logged in")
+      setUser({ name, email })
+    }else{
+      console.log('details wrong')
+      setError("Details do not match")
+    }
   }
 
   const Logout = () => {
@@ -56,26 +46,17 @@ function Login() {
 
   return (
     <div className="login">
-
-      {(user.email !== "") ? (
+      {(email !== "") ? (
         <div>
-          <h2>Welcome, <span>{user.name}</span></h2>
+          <h2>Welcome, <span>{name}</span></h2>
           <button onClick={Logout}>Logout</button>
         </div>
       ) : (
         <div>
-
         <LoginForm login={Login} error={error} />
-
         <button onClick={fetchData}>FETCHDATA</button>
-
         </div>
-
-        
       )}
-
-
-
     </div>
   )
 }
