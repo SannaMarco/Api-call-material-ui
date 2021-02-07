6193c4c42d855374a0e9f508750c84e67103d86f
Marco Sanna <sanna.marco99@outlook.it>
2021-02-07 22:11:23 +0100

    add react hook form

--- src/pages/login/components/LoginForm.jsx
+++ src/pages/login/components/LoginForm.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import Button from '@material-ui/core/Button';
 import {TextField, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
+import {useForm} from 'react-hook-form'
+import WaringIcon from '@material-ui/icons/Warning'
 
 
 
@@ -11,49 +13,66 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     alignItems:'center'
   },
+  title:{
+
+  },
+  inputFields: {
+    width: '80%',
+    margin: '20px 0'
+  }
 }));
 
 function LoginForm({ login, error }) {
-  const [details, setDetails] = useState({ name: "", email: "", password: "" });
   const classes = useStyles();
+  const {register, handleSubmit, errors} = useForm();
 
-  const submitHandler = (e) => {
-    e.preventDefault();
+  const onSubmit = data =>{
+    console.log(data.name);
+    login(data);
 
-    login(details);
-  };
+
+  }
 
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className={classes.root}>
         <Typography variant="h4">Login</Typography>
 
         {error !== "" ? <div className="error">{error}</div> : ""}
 
         <TextField
+            className={classes.inputFields}
             variant="outlined"
             size="small"
             label="Nome"
-            onChange={(e) => setDetails({ ...details, name: e.target.value })}
-            value={details.name}
+            name='name'  
+            inputRef={register({required: true})}      
         />
+        {errors.name && <p><WaringIcon/>  This is required</p>}
 
         <TextField
+            className={classes.inputFields}
             variant="outlined"
             size="small"
-            label="Email"
-            onChange={(e) => setDetails({ ...details, email: e.target.value })}
-            value={details.email}
+            label="Email"  
+            name='email' 
+            inputRef={register({required: true})}         
         />
 
+        {errors.email && <p><WaringIcon/>  This is required</p>}
+
+
         <TextField
+            className={classes.inputFields}
             variant="outlined"
             size="small"
             label="Password"
+            name='password'
             type="password"
-            onChange={(e) => setDetails({ ...details, password: e.target.value })}
-            value={details.password}
+            inputRef={register({required: true})}
         />
+         {errors.password && <p><WaringIcon/>  This is required</p>}
+
 
         <Button type="submit" variant="contained" color="primary">Login</Button>
         
